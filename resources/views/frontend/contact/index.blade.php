b500778d0578c753a8abd0019335afd21073e575
Sangeetha-G-Roka <sangeetharoka41@gmail.com>
2026-05-31 16:20:46 +0545

    q

--- resources/views/frontend/contact/index.blade.php
+++ resources/views/frontend/contact/index.blade.php
@@ -212,7 +212,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
-                        <span>Gon bhyabo DiOce, Kathmandu</span>
+                        <span> Kathmandu</span>
                     </div>
 
                     <div class="ct-details__item">
@@ -220,7 +220,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                         </svg>
-                        <span>+337 8217.0600</span>
+                        <span>+977 1234567890</span>
                     </div>
 
                     <div class="ct-details__item">
@@ -228,7 +228,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                         </svg>
-                        <span>asncl@lnstpepseit.com</span>
+                        <span>ApexNepal@gmail.com</span>
                     </div>
 
                     <div class="ct-details__item">
@@ -236,7 +236,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
                         </svg>
-                        <span>slotinepaepat.com</span>
+                        <span>ApexNepal.com</span>
                     </div>
                 </div>
             </div>
